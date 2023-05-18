24edf181900785d3f24765f949f5dde84af13b62
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-05-19 04:29:56 +0600

    create gallery api

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // gallery
+    const toysGallery = client.db('toysGalleryDB').collection('toys')
+    
+    app.get('/toysGallery', async(req, res) => {
+        const result = await toysGallery.find().toArray();
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
